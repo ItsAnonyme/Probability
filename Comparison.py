684d30e135ec009c209674c7b499d03cc60603d5
ItsAnonyme <rasmushaun01@gmail.com>
2025-09-25 21:49:42 +0200

    Update Comparison.py

--- Comparison.py
+++ Comparison.py
@@ -3,16 +3,21 @@ from PoissonDistribution import prediction_poisson
 from NegativeBinomialDistribution import prediction_nbinomial
 from decimal import Decimal
 from nbinomTest import prediction_nbinomial2
+import numpy as np
 
-List = pd.read_csv("Dataset-small.csv", skiprows=range(0, 3205), engine="python")
+List = pd.read_csv("Dataset-small.csv", skiprows=range(0, 3204), engine="python")
 
 def compare_prediction_poisson():
-    temp_poisson = 0
-    for i in range(1, len(List)):
-        #print("Input row:", List.iloc[i])
-        if prediction_poisson(List.iloc[i, 2], List.iloc[i, 3]) == List.iloc[i, 6]:
-            temp_poisson += 1
-    print(f"The Poisson Distribution got {round(Decimal((temp_poisson/len(List)) * 100), 10)} % correct")
+    dict = {}
+    for j in np.arange(0, 0.1, 0.0005):
+        temp_poisson = 0
+        for i in range(1, len(List)):
+            if prediction_poisson(List.iloc[i, 2], List.iloc[i, 3], j) == List.iloc[i, 6]:
+                temp_poisson += 1
+            dict[j] = temp_poisson/len(List)
+        #print(f"The Poisson Distribution got {round(Decimal((temp_poisson/len(List)) * 100), 10)} % correct")
+    print(dict)
+    print(max(dict, key=dict.get))
 
 def compare_prediction_nbinomial():
     temp_nbinom = 0
