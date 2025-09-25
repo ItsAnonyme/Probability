15fc6d387a08f7233985bf78f12a60b4f46a14d2
ItsAnonyme <rasmushaun01@gmail.com>
2025-09-26 01:11:28 +0200

    Update Comparison.py

--- Comparison.py
+++ Comparison.py
@@ -5,16 +5,17 @@ from decimal import Decimal
 from nbinomTest import prediction_nbinomial2
 import numpy as np
 
-List = pd.read_csv("Dataset-small.csv", skiprows=range(0, 3204), engine="python")
+List = pd.read_csv("premier_league_all_seasons_cleaned.csv", skiprows=range(0, 12312), engine="python")
 
 def compare_prediction_poisson():
     dict = {}
-    for j in np.arange(0, 0.1, 0.0005):
+    for j in np.linspace(0, 0.1, 50):
         temp_poisson = 0
-        for i in range(1, len(List)):
-            if prediction_poisson(List.iloc[i, 2], List.iloc[i, 3], j) == List.iloc[i, 6]:
+        for i in range(0, len(List)):
+            if prediction_poisson(List.iloc[i, 1], List.iloc[i, 2], j) == List.iloc[i, 5]:
                 temp_poisson += 1
-            dict[j] = temp_poisson/len(List)
+        dict[j] = temp_poisson/len(List)
+        print(f"{j}: {temp_poisson/len(List)}")
         #print(f"The Poisson Distribution got {round(Decimal((temp_poisson/len(List)) * 100), 10)} % correct")
     print(dict)
     print(max(dict, key=dict.get))
